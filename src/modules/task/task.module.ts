583cf5e27fe4c0dc4837c9ab159242c6d3c5a1f6
Raby001 <hanraby001@gmail.com>
2026-02-20 08:45:27 +0700

    Set up nestjs

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './task.controller';
 import { TaskService } from './task.service';
+import { Task } from './task.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Task]), UserModule],
   controllers: [TasksController],
   providers: [TaskService],
-  exports: [],
   // Add any other necessary configurations or modules
 })
 export class TaskModule {}
